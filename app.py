72503bf96f830345d535ae8a67fff02d94a625a3
magic3xx <darkmagic3xx@gmail.com>
2024-10-20 22:42:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,5 @@ def check_key_details():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Allow external access
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+
